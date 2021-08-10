11f28dfed1c9c2bca9fc69d36236e850097721e2
xuchaobei <xuchao_zju@yeah.net>
2021-08-10 20:45:29 +0800

    feat: component-props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ArticleList from './components/ArticleList';
+
+const articles = [
+  { id: 1, title: 'Article 1' },
+  { id: 2, title: 'Article 2' },
+  { id: 3, title: 'Article 3' },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ArticleList articles={articles} />;
 }
 
 export default App;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// const ArticleList = (props) => {
+//   const { articles } = props;
+//   return articles.map((item) => {
+//     const { title } = item;
+//     return <div> {title}</div>;
+//   });
+
+//   // 列表渲染，列表中的每一项要设置 key
+//   // return articles.map((item) => {
+//   //   const { id, title } = item;
+//   //   return <div key={id}> {title}</div>;
+//   // });
+
+// };
+
+class ArticleList extends React.Component {
+  render() {
+    const { articles } = this.props;
+    return articles.map((item) => {
+      const { title } = item;
+      return <div> {title}</div>;
+    });
+  }
+}
+
+export default ArticleList;
