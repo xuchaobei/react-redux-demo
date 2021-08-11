2571d9d74b73680b3a56ec26a3d43057f32df94d
xuchaobei <xuchao_zju@yeah.net>
2021-08-11 12:52:55 +0800

    feat: state hook

--- src/components/AddPostForm/index.js
+++ src/components/AddPostForm/index.js
@@ -1,41 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css'
 
-/** 受控组件 */
-class AddPostForm extends React.Component{
+/** hooks 版本 */
+function AddPostForm(props){
 
-  constructor(props){
-    super(props);
-    this.state = {
-      title: '',
-      content: ''
-    }
-  }
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-
-  handleChange = (e) => {
+  const handleChange = (e) => {
     if (e.target.name === 'title') {
-      this.setState({
-        title: e.target.value
-      })
+      setTitle(e.target.value)
 
     } else if (e.target.name === 'content') {
-      this.setState({
-        content: e.target.value
-      })
+      setContent(e.target.value)
     }
   };
 
-  handleSave = async () => {
+  const handleSave = async () => {
     const {title, content} = this.state;
     if (title.length > 0 && content.length > 0) {
       console.log(title, content)
     }
   };
 
-  render() {
-    const {title, content} = this.props;
-    return (
+  return (
       <section>
         <h2>Add a New Post</h2>
         <form>
@@ -45,23 +33,85 @@ class AddPostForm extends React.Component{
             id="title"
             name="title"
             value={title}
-            onChange={this.handleChange}
+            onChange={handleChange}
           />
           <label htmlFor="content">Content:</label>
           <textarea
             id="content"
             name="content"
             value={content}
-            onChange={this.handleChange}
+            onChange={handleChange}
           />
-          <button type="button" className="button" onClick={this.handleSave}>
+          <button type="button" className="button" onClick={handleSave}>
             Save Post
           </button>
         </form>
       </section>
     );
-  }
 };
+
+
+/** 受控组件 */
+// class AddPostForm extends React.Component{
+
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       title: '',
+//       content: ''
+//     }
+//   }
+
+
+//   handleChange = (e) => {
+//     if (e.target.name === 'title') {
+//       this.setState({
+//         title: e.target.value
+//       })
+
+//     } else if (e.target.name === 'content') {
+//       this.setState({
+//         content: e.target.value
+//       })
+//     }
+//   };
+
+//   handleSave = async () => {
+//     const {title, content} = this.state;
+//     if (title.length > 0 && content.length > 0) {
+//       console.log(title, content)
+//     }
+//   };
+
+//   render() {
+//     const {title, content} = this.props;
+//     return (
+//       <section>
+//         <h2>Add a New Post</h2>
+//         <form>
+//           <label htmlFor="title">Post Title:</label>
+//           <input
+//             type="text"
+//             id="title"
+//             name="title"
+//             value={title}
+//             onChange={this.handleChange}
+//           />
+//           <label htmlFor="content">Content:</label>
+//           <textarea
+//             id="content"
+//             name="content"
+//             value={content}
+//             onChange={this.handleChange}
+//           />
+//           <button type="button" className="button" onClick={this.handleSave}>
+//             Save Post
+//           </button>
+//         </form>
+//       </section>
+//     );
+//   }
+// };
 
 
 /** 非受控组件 */
